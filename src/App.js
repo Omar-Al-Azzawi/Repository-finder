9c0b7036dab519dbe0dc71ebf913f55b57c7af70
Omar Alazzawi <omaralazzawi@Omars-MBP.home>
2021-06-20 22:13:46 +0300

    create repository finder app using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
+import RepoDetails from "../src/Repo-details"
 
 function App() {
+const[username, setUserName] = useState("");
+const[loading, setLoading] = useState(false);
+const[repos, setRepos] = useState([]);
+const[details, setDetails] = useState({});
+const[detailsLoading, setDetailsLoading] = useState(false);
+
+
+useEffect(() => {
+  setRepos([]);
+  setDetails({})
+}, [username]);
+
+function handleSubmit(e){
+  e.preventDefault();
+  searchRepos();
+};
+
+function searchRepos(){
+  setLoading(true);
+  axios({
+    method: "get",
+    url: `https://api.github.com/users/${username}/repos`,
+  }).then(res => {
+    setLoading(false);
+    setRepos(res.data);
+  });
+}
+
+function renderRepo(repo){
+  return(
+    <div className="row" onClick={() => getDetails(repo.name)} key={repo.id}>
+      <h2 className="repo-name">
+        {repo.name}
+      </h2>
+    </div>
+  )
+}
+function getDetails(repoName){
+  setDetailsLoading(true);
+  axios({
+    method: "get",
+    url: `https://api.github.com/repos/${username}/${repoName}`,  
+  }).then(res =>{
+    setDetailsLoading(false);
+    setDetails(res.data);
+  });
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="landing-page-container">
+        <div className="left-side">
+        <h1>Github Repository Finder</h1>
+          <form className="form">
+          <input 
+            className="input"
+            value={username}
+            placeholder="Github Username"
+            onChange={e => setUserName(e.target.value)}
+          />
+          <button className="button"
+          onClick={handleSubmit}>{ loading ? "Serching..." : "Search" }</button>
+          </form>
+          <div className="results-container">
+            {repos.map(renderRepo)}
+          </div>
+        </div>
+        <RepoDetails details={details} loading={detailsLoading} />
+      </div>
     </div>
   );
 }
